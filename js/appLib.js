fee0439cb70f3169bed86e2da54b765637fe49c5
romeshRaghavan <magneto.1990@gmail.com>
2017-01-04 16:51:52 +0530

    Add files via upload

--- js/appLib.js
+++ js/appLib.js
@@ -83,7 +83,6 @@ function goBack() {
 	}
  
 function goBackEvent() {
-	alert("goBackEvent")
 	var currentUser=getUserID();
 	var pageRef=defaultPagePath+'prInvoice.html';
 	var loginPath=defaultPagePath+'loginPage.html';
@@ -96,8 +95,6 @@ function goBackEvent() {
 		//To check if the page that needs to be displayed is login page. So 'historylength-2'
 		var historylength=appPageHistory.length;
 		var goToPage=appPageHistory[historylength-2];
-		alert(goToPage+" goToPage")
-		alert(appPageHistory+" appPageHistory")
 
 		if(goToPage!==null && goToPage==loginPath){
 			return 0;
@@ -109,32 +106,27 @@ function goBackEvent() {
 				//navigator.notification.confirm("Are you sure want to exit from App?", onConfirmExit, "Confirmation", "Yes,No");
 			}else{
 				var pg=appPageHistory[len-1];
-				alert(pg+" pg")
 				if(pg=="app/pages/addPurchaseReq.html"){ 
-					alert("addPurchaseReq")
 					 backBtn = true;
 					 pageRef=defaultPagePath+'addPurchaseReq.html';
 					 headerBackBtn=defaultPagePath+'backToHomeStepOneImg.html';
 					 j('#mainHeader').load(headerBackBtn);
 					 j('#mainContainer').load(pageRef);						
 				}else if(pg=="app/pages/addPurchaseReqScreen2.html"){
-					alert("addPurchaseReqScreen2")
 					 backBtn = true;
 					 pageRef=defaultPagePath+'addPurchaseReqScreen2.html';
 					 headerBackBtn=defaultPagePath+'backToHomeStepTwoImg.html';	
 					 j('#mainHeader').load(headerBackBtn);
 					 j('#mainContainer').load(pageRef);	
 				}else if(pg=="app/pages/addPurchaseReqScreen3.html"){
-					alert("addPurchaseReqScreen3")
 					 backBtn = true;
 					 pageRef=defaultPagePath+'prInvoice.html';
-					 headerBackBtn=defaultPagePath+'expenzingImagePage.html';	
+					 headerBackBtn=defaultPagePath+'expenzingImageWithSyncPage.html';	
 					 j('#mainHeader').load(headerBackBtn);
 					 j('#mainContainer').load(pageRef);	
 				}else if(pg=="app/pages/prInvoice.html"){
-					alert("prInvoice")
 					 pageRef=defaultPagePath+'prInvoice.html';
-					 headerBackBtn=defaultPagePath+'expenzingImagePage.html';	
+					 headerBackBtn=defaultPagePath+'expenzingImageWithSyncPage.html';	
 					 j('#mainHeader').load(headerBackBtn);
 					 j('#mainContainer').load(pageRef);	
 				}
@@ -1943,6 +1935,7 @@ function saveEasyPr(){
          data: JSON.stringify(jsonToAppSend),
          success: function(data) {
          	if (data.status == 'Success'){
+         	var pageRef=defaultPagePath+'success.html';
         	 j('#mainHeader').load(headerBackBtn);
              j('#mainContainer').load(pageRef);
              successMessage = data.Message;
